bea800a20c3e64dc3d7cc0fd42b9c3943bccb1fe
rocio <rocio.carratala@gmail.com>
2015-03-31 11:34:59 +0200

    Correct fixed size cubes extrusion from starting cube.

--- Source/ProceduralMesh/ProceduralCubeActor.cpp
+++ Source/ProceduralMesh/ProceduralCubeActor.cpp
@@ -16,33 +16,27 @@ AProceduralCubeActor::AProceduralCubeActor(const class FPostConstructInitializeP
 	: Super(PCIP)
 {
 
-
 	mesh = PCIP.CreateDefaultSubobject<UProceduralMeshComponent>(this, TEXT("ProceduralCube"));
 	
 	// Apply a simple material directly using the VertexColor as its BaseColor input
 	static ConstructorHelpers::FObjectFinder<UMaterialInterface> Material(TEXT("Material'/Game/Materials/BaseColor.BaseColor'"));
+	
 	// TODO Apply a real material with textures, using UVs
-	// static ConstructorHelpers::FObjectFinder<UMaterialInterface> Material(TEXT("Material'/Game/Materials/M_Concrete_Poured.M_Concrete_Poured'"));
 	mesh->SetMaterial(0, Material.Object);
-
-	// Generate a cube
-	TArray<FProceduralMeshTriangle> triangles;
-	GenerateCube(100.f, triangles);
-	mesh->SetProceduralMeshTriangles(triangles);
 
 	RootComponent = mesh;
 
-	// Enable OnHover, OnExitHover
-//	this->OnBeginCursorOver.AddDynamic(this, &AProceduralCubeActor::PlayerOnHover);
-//	this->OnEndCursorOver.AddDynamic(this, &AProceduralCubeActor::PlayerOnExitHover);
-
 }
 
-void AProceduralCubeActor::GenerateCube(const float& InSize, TArray<FProceduralMeshTriangle>& OutTriangles)
+//void AProceduralCubeActor::GenerateCube(const float& InSize, TArray<FProceduralMeshTriangle>& OutTriangles)
+void AProceduralCubeActor::GenerateCube(FVector StarterP0Location, float XSize, float YSize, float ZSize)
 {
 
-	GenerateCubePs(InSize);
+	TArray<FProceduralMeshTriangle> OutTriangles;
+
+	GenerateCubePs(StarterP0Location, XSize, YSize, ZSize);
 	GenerateCubeVs();
+	SetCubeVColors(FColor::Green);
 	FProceduralMeshTriangle t1, t2;
 
 	// front face
@@ -75,32 +69,44 @@ void AProceduralCubeActor::GenerateCube(const float& InSize, TArray<FProceduralM
 	OutTriangles.Add(t1);
 	OutTriangles.Add(t2);
 
+	mesh->SetProceduralMeshTriangles(OutTriangles);
 
 }
 
-void AProceduralCubeActor::GenerateCubePs(const float& InSize)
+void AProceduralCubeActor::GenerateCubePs(FVector P0Coords, float XSize, float YSize, float ZSize)
 {
-	p0 = FVector(0.f, 0.f, 0.f);
-	p1 = FVector(0.f, 0.f, InSize);
-	p2 = FVector(InSize, 0.f, InSize);
-	p3 = FVector(InSize, 0.f, 0.f);
-	p4 = FVector(InSize, InSize, 0.f);
-	p5 = FVector(InSize, InSize, InSize);
-	p6 = FVector(0.f, InSize, InSize);
-	p7 = FVector(0.f, InSize, 0.f);
+	p0 = P0Coords + FVector(0.f, 0.f, 0.f);
+	p1 = P0Coords + FVector(0.f, 0.f, ZSize);
+	p2 = P0Coords + FVector(XSize, 0.f, ZSize);
+	p3 = P0Coords + FVector(XSize, 0.f, 0.f);
+	p4 = P0Coords + FVector(XSize, YSize, 0.f);
+	p5 = P0Coords + FVector(XSize, YSize, ZSize);
+	p6 = P0Coords + FVector(0.f, YSize, ZSize);
+	p7 = P0Coords + FVector(0.f, YSize, 0.f);
 }
 
 void AProceduralCubeActor::GenerateCubeVs()
 {
-	FColor VertexColor = FColor::Red;
-	v0.Position = p0; v0.Id = 0; v0.Color = VertexColor;
-	v1.Position = p1; v1.Id = 1; v1.Color = VertexColor;
-	v2.Position = p2; v2.Id = 2; v2.Color = VertexColor;
-	v3.Position = p3; v3.Id = 3; v3.Color = VertexColor;
-	v4.Position = p4; v4.Id = 4; v4.Color = VertexColor;
-	v5.Position = p5; v5.Id = 5; v5.Color = VertexColor;
-	v6.Position = p6; v6.Id = 6; v6.Color = VertexColor;
-	v7.Position = p7; v7.Id = 7; v7.Color = VertexColor;
+	v0.Position = p0; v0.Id = 0;
+	v1.Position = p1; v1.Id = 1;
+	v2.Position = p2; v2.Id = 2;
+	v3.Position = p3; v3.Id = 3;
+	v4.Position = p4; v4.Id = 4;
+	v5.Position = p5; v5.Id = 5;
+	v6.Position = p6; v6.Id = 6;
+	v7.Position = p7; v7.Id = 7;
+}
+
+void AProceduralCubeActor::SetCubeVColors(FColor VertexColor)
+{
+	v0.Color = VertexColor;
+	v1.Color = VertexColor;
+	v2.Color = VertexColor;
+	v3.Color = VertexColor;
+	v4.Color = VertexColor;
+	v5.Color = VertexColor;
+	v6.Color = VertexColor;
+	v7.Color = VertexColor;
 }
 
 void AProceduralCubeActor::GenerateCubeFace(FProceduralMeshVertex GivenV0, FProceduralMeshVertex GivenV1, FProceduralMeshVertex GivenV2, FProceduralMeshVertex GivenV3, FProceduralMeshTriangle& t1, FProceduralMeshTriangle& t2)
@@ -120,6 +126,148 @@ void AProceduralCubeActor::GenerateCubeFace(FProceduralMeshVertex GivenV0, FProc
 	t2.Vertex2 = GivenV3;
 	
 	//return CalculateArrowLocationFromFaceVertex(GivenV0, GivenV1, GivenV2, GivenV3);
+}
+
+void AProceduralCubeActor::ExtrusionFromGivenFaceVertexes(AProceduralCubeActor* NewCube, TArray<FProceduralMeshVertex> FaceVertexes)
+{
+
+	//GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Red, TEXT("Begin extrusion!"));
+	FRotator ParentCubeRotation = this->GetActorRotation();
+	FVector ParentCubeLocation = this->GetActorLocation();
+	float ParentXSize = abs(p3.X - p0.X);
+	float ParentYSize = abs(p7.Y - p0.Y);
+	float ParentZSize = abs(p1.Z - p0.Z);
+
+	//AProceduralCubeActor* NewCube = GetWorld()->SpawnActor<AProceduralCubeActor>(AProceduralCubeActor::StaticClass());
+	FProceduralMeshVertex FVertex0 = FaceVertexes[0];
+	FProceduralMeshVertex FVertex1 = FaceVertexes[1];
+	FProceduralMeshVertex FVertex2 = FaceVertexes[2];
+	FProceduralMeshVertex FVertex3 = FaceVertexes[3];
+
+	/*
+	float SizeX_1 = abs(FVertex0.Position.X - FVertex1.Position.X);
+	float SizeX_2 = abs(FVertex0.Position.X - FVertex2.Position.X);
+	float SizeX_3 = abs(FVertex0.Position.X - FVertex3.Position.X);
+	float ParentXSize;
+	if (SizeX_1 >= SizeX_2 && SizeX_1 >= SizeX_3) { ParentXSize = SizeX_1; }
+	else if (SizeX_2 >= SizeX_1 && SizeX_2 >= SizeX_3) { ParentXSize = SizeX_2; }
+	else { ParentXSize = SizeX_3; }
+	//float ParentXSize = (SizeX_1 > SizeX_2) ? (SizeX_1) : SizeX_2;
+	//ParentXSize = (ParentXSize > SizeX_3) ? ParentXSize : SizeX_3;
+
+	float SizeY_1 = abs(FVertex0.Position.Y - FVertex1.Position.Y);
+	float SizeY_2 = abs(FVertex0.Position.Y - FVertex2.Position.Y);
+	float SizeY_3 = abs(FVertex0.Position.Y - FVertex3.Position.Y);
+	float ParentYSize;
+	if (SizeY_1 >= SizeY_2 && SizeY_1 >= SizeY_3) { ParentYSize = SizeY_1; }
+	else if (SizeY_2 >= SizeY_1 && SizeY_2 >= SizeY_3) { ParentYSize = SizeY_2; }
+	else { ParentYSize = SizeY_3; }
+	//float ParentYSize = (SizeY_1 > SizeY_2) ? (SizeY_1) : SizeY_2;
+	//ParentYSize = (ParentYSize > SizeY_3) ? ParentYSize : SizeY_3;
+
+	float SizeZ_1 = abs(FVertex0.Position.Z - FVertex1.Position.Z);
+	float SizeZ_2 = abs(FVertex0.Position.Z - FVertex2.Position.Z);
+	float SizeZ_3 = abs(FVertex0.Position.Z - FVertex3.Position.Z);
+	float ParentZSize;
+	if (SizeZ_1 >= SizeZ_2 && SizeZ_1 >= SizeZ_3) { ParentZSize = SizeZ_1; }
+	else if (SizeZ_2 >= SizeZ_1 && SizeZ_2 >= SizeZ_3) { ParentZSize = SizeZ_2; }
+	else { ParentZSize = SizeZ_3; }
+	//float ParentZSize = (SizeZ_1 > SizeZ_2) ? (SizeZ_1) : SizeZ_2;
+	//ParentZSize = (ParentZSize > SizeZ_3) ? ParentZSize : SizeZ_3;
+	*/
+
+	//GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Red, FString::Printf(TEXT("Given Id's: %f %f %f %f"), FVertex0.Id, FVertex1.Id, FVertex2.Id, FVertex3.Id));
+	TArray<int32> frontFace;	frontFace.Add(v0.Id);	frontFace.Add(v1.Id);	frontFace.Add(v2.Id);	frontFace.Add(v3.Id);
+	TArray<int32> backFace;		backFace.Add(v4.Id);	backFace.Add(v5.Id);	backFace.Add(v6.Id);	backFace.Add(v7.Id);
+	TArray<int32> leftFace;		leftFace.Add(v7.Id);	leftFace.Add(v6.Id);	leftFace.Add(v1.Id);	leftFace.Add(v0.Id);
+	TArray<int32> rightFace;	rightFace.Add(v3.Id);	rightFace.Add(v2.Id);	rightFace.Add(v5.Id);	rightFace.Add(v4.Id);
+	TArray<int32> topFace;		topFace.Add(v1.Id);		topFace.Add(v6.Id);		topFace.Add(v5.Id);		topFace.Add(v2.Id);
+	TArray<int32> bottomFace;	bottomFace.Add(v3.Id);	bottomFace.Add(v4.Id);	bottomFace.Add(v7.Id);	bottomFace.Add(v0.Id);
+
+	// Given Vertexes are Front Face Vertexes
+	if (VectorContainsElement(frontFace, FVertex0.Id) && VectorContainsElement(frontFace, FVertex1.Id) && VectorContainsElement(frontFace, FVertex2.Id) && VectorContainsElement(frontFace, FVertex3.Id))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Yellow, TEXT("Extruding Front Face"));
+		NewCube->GenerateCubePs(FVector(p0.X, p0.Y - ParentYSize, p0.Z), ParentXSize, ParentYSize, ParentZSize);
+	}
+	// Given Vertexes are Back Face Vertexes
+	else if (VectorContainsElement(backFace, FVertex0.Id) && VectorContainsElement(backFace, FVertex1.Id) && VectorContainsElement(backFace, FVertex2.Id) && VectorContainsElement(backFace, FVertex3.Id))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Yellow, TEXT("Extruding Back Face"));
+		NewCube->GenerateCubePs(p7, ParentXSize, ParentYSize, ParentZSize);
+	}
+	// Given Vertexes are Left Face Vertexes
+	else if (VectorContainsElement(leftFace, FVertex0.Id) && VectorContainsElement(leftFace, FVertex1.Id) && VectorContainsElement(leftFace, FVertex2.Id) && VectorContainsElement(leftFace, FVertex3.Id))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Yellow, TEXT("Extruding Left Face"));
+		NewCube->GenerateCubePs(FVector(p0.X - ParentXSize, p0.Y, p0.Z), ParentXSize, ParentYSize, ParentZSize);
+	}
+	// Given Vertexes are Right Face Vertexes
+	else if (VectorContainsElement(rightFace, FVertex0.Id) && VectorContainsElement(rightFace, FVertex1.Id) && VectorContainsElement(rightFace, FVertex2.Id) && VectorContainsElement(rightFace, FVertex3.Id))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Yellow, TEXT("Extruding Right Face"));
+		NewCube->GenerateCubePs(p3, ParentXSize, ParentYSize, ParentZSize);
+	}
+	// Given Vertexes are Top Face Vertexes
+	else if (VectorContainsElement(topFace, FVertex0.Id) && VectorContainsElement(topFace, FVertex1.Id) && VectorContainsElement(topFace, FVertex2.Id) && VectorContainsElement(topFace, FVertex3.Id))
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Yellow, TEXT("Extruding Top Face"));
+		NewCube->GenerateCubePs(p1, ParentXSize, ParentYSize, ParentZSize);
+	}
+	// Given Vertexes are Bottom Face Vertexes
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Yellow, TEXT("Extruding Bottom Face"));
+		NewCube->GenerateCubePs(FVector(p0.X, p0.Y, p0.Z - ParentZSize), ParentXSize, ParentYSize, ParentZSize);
+	}
+
+	NewCube->GenerateCubeVs();
+
+	NewCube->SetCubeVColors(FColor::Red);
+
+	FProceduralMeshTriangle t1, t2;
+	TArray<FProceduralMeshTriangle> OutTriangles;
+
+	// front face
+	NewCube->GenerateCubeFace(NewCube->v0, NewCube->v1, NewCube->v2, NewCube->v3, t1, t2);
+	OutTriangles.Add(t1);
+	OutTriangles.Add(t2);
+
+	// back face
+	NewCube->GenerateCubeFace(NewCube->v4, NewCube->v5, NewCube->v6, NewCube->v7, t1, t2);
+	OutTriangles.Add(t1);
+	OutTriangles.Add(t2);
+
+	// left face
+	NewCube->GenerateCubeFace(NewCube->v7, NewCube->v6, NewCube->v1, NewCube->v0, t1, t2);
+	OutTriangles.Add(t1);
+	OutTriangles.Add(t2);
+
+	// right face
+	NewCube->GenerateCubeFace(NewCube->v3, NewCube->v2, NewCube->v5, NewCube->v4, t1, t2);
+	OutTriangles.Add(t1);
+	OutTriangles.Add(t2);
+
+	// top face
+	NewCube->GenerateCubeFace(NewCube->v1, NewCube->v6, NewCube->v5, NewCube->v2, t1, t2);
+	OutTriangles.Add(t1);
+	OutTriangles.Add(t2);
+
+	// bottom face
+	NewCube->GenerateCubeFace(NewCube->v3, NewCube->v4, NewCube->v7, NewCube->v0, t1, t2);
+	OutTriangles.Add(t1);
+	OutTriangles.Add(t2);
+
+	NewCube->SetActorRotation(ParentCubeRotation);
+	NewCube->SetActorLocation(ParentCubeLocation);
+
+	NewCube->mesh->SetProceduralMeshTriangles(OutTriangles);
+
+	ExtrudedCubes.Add(NewCube);
+
+	GEngine->AddOnScreenDebugMessage(-1, 20, FColor::Red, FString::Printf(TEXT("Sizes: %f %f %f"), ParentXSize, ParentYSize, ParentZSize));
+	//GEngine->AddOnScreenDebugMessage(-1, 15, FColor::Red, TEXT("Extrusion done!"));
+
 }
 
 // ------------------------------------------------- End - GENERATION FUNCTIONS ------------------------------------------------- \\
@@ -188,6 +336,17 @@ FProceduralMeshVertex AProceduralCubeActor::MoveVertexP0(FVector Direction, FPro
 FVector AProceduralCubeActor::FindAndMoveVertex(FVector MovementDirection, FProceduralMeshVertex VertexToMove, TArray<FProceduralMeshTriangle>& CurrentTriangles)
 {
 	FVector NewVertexPosition = VertexToMove.Position;
+
+	if (VertexToMove.Id == 0) { p0 = p0 + MovementDirection; }
+	else if (VertexToMove.Id == 1) { p1 = p1 + MovementDirection; }
+	else if (VertexToMove.Id == 2) { p2 = p2 + MovementDirection; }
+	else if (VertexToMove.Id == 3) { p3 = p3 + MovementDirection; }
+	else if (VertexToMove.Id == 4) { p4 = p4 + MovementDirection; }
+	else if (VertexToMove.Id == 5) { p5 = p5 + MovementDirection; }
+	else if (VertexToMove.Id == 6) { p6 = p6 + MovementDirection; }
+	else if (VertexToMove.Id == 7) { p7 = p7 + MovementDirection; }
+
+
 	bool found = false;
 	for (int i = 0; i < CurrentTriangles.Num(); i++)
 	{
@@ -361,7 +520,6 @@ FRotator AProceduralCubeActor::GetOrtogonalFaceDirectionFromFaceVertex(FVector G
 
 }
 
-
 // ------------------------------------------------- End - FACE ARROWS FUNCTIONS ------------------------------------------------- \\
 // ------------------- o ------------------- o ------------------- o ------------------- o ------------------- o ------------------- o ------------------- \\
 
@@ -417,6 +575,19 @@ FVector AProceduralCubeActor::CrossProd(FVector U, FVector V)
 	return FVector(U[1] * V[2] - U[2] * V[1], U[2] * V[0] - U[0] * V[2], U[0] * V[1] - U[1] * V[0]);
 }
 
+bool AProceduralCubeActor::VectorContainsElement(TArray<int32> GivenVector, int32 GivenElement)
+{
+	bool ElementFound = false;
+	for (int32 i = 0; i < GivenVector.Num() && !(ElementFound); i++)
+	{
+		if (GivenVector[i] == GivenElement)
+		{
+			ElementFound = true;
+		}
+	}
+	return ElementFound;
+}
+
 // ------------------------------------------------ End - AUX FUNCTIONS ------------------------------------------------ \\
 // ------------------- o ------------------- o ------------------- o ------------------- o ------------------- o ------------------- o ------------------- \\
 
@@ -429,47 +600,47 @@ FVector AProceduralCubeActor::CrossProd(FVector U, FVector V)
 
 FVector AProceduralCubeActor::RotateOnX(FVector Point, float Angle)
 {
-	return FVector(Point.X, Point.Y*cos(Angle) - Point.Z*sin(Angle), Point.Z*cos(Angle) + Point.Y*sin(Angle));
+	return FVector( 180 * (Point.X ) / PI, 180 * (Point.Y*cos(Angle) - Point.Z*sin(Angle)) / PI, 180 * (Point.Z*cos(Angle) + Point.Y*sin(Angle)) / PI);
 }
 
 FVector AProceduralCubeActor::UnRotateOnX(FVector Point, float Angle)
 {
-	return FVector(Point.X, Point.Y*cos(Angle) + Point.Z*sin(Angle), Point.Z*cos(Angle) - Point.Y*sin(Angle));
+	return FVector(180 * (Point.X) / PI, 180 * (Point.Y*cos(Angle) + Point.Z*sin(Angle)) / PI, 180* (Point.Z*cos(Angle) - Point.Y*sin(Angle))/ PI);
 }
 
 FVector AProceduralCubeActor::RotateOnY(FVector Point, float Angle)
 {
-	return FVector(Point.X*cos(Angle) + Point.Z*sin(Angle), Point.Y, Point.Z*cos(Angle) - Point.X*sin(Angle));
+	return FVector(180 * (Point.X*cos(Angle) + Point.Z*sin(Angle)) / PI, 180 * (Point.Y) / PI, 180 * (Point.Z*cos(Angle) - Point.X*sin(Angle)) / PI);
 }
 
 FVector AProceduralCubeActor::UnRotateOnY(FVector Point, float Angle)
 {
-	return FVector(Point.X*cos(Angle) - Point.Z*sin(Angle), Point.Y, Point.Z*cos(Angle) + Point.X*sin(Angle));
+	return FVector(180 * (Point.X*cos(Angle) - Point.Z*sin(Angle)) / PI, 180 * (Point.Y) / PI, 180 * (Point.Z*cos(Angle) + Point.X*sin(Angle))/ PI);
 }
 
 FVector AProceduralCubeActor::RotateOnZ(FVector Point, float Angle)
 {
-	return FVector(Point.X*cos(Angle) - Point.Y*sin(Angle), Point.Y*cos(Angle) + Point.X*sin(Angle), Point.Z);
+	return FVector(180 * (Point.X*cos(Angle) - Point.Y*sin(Angle)) / PI, 180 * (Point.Y*cos(Angle) + Point.X*sin(Angle)) / PI, 180 * (Point.Z) / PI);
 }
 
 FVector AProceduralCubeActor::UnRotateOnZ(FVector Point, float Angle)
 {
-	return FVector(Point.X*cos(Angle) + Point.Y*sin(Angle), Point.Y*cos(Angle) - Point.X*sin(Angle), Point.Z);
+	return FVector(180 * (Point.X*cos(Angle) + Point.Y*sin(Angle)) / PI, 180 * (Point.Y*cos(Angle) - Point.X*sin(Angle)) / PI, 180 * (Point.Z)/ PI);
 }
 
 FVector AProceduralCubeActor::RotateOnXYZ(FVector Point, float AngleX, float AngleY, float AngleZ)
 {
-	return FVector(Point.X*cos(AngleY)*cos(AngleZ) + Point.Y*(cos(AngleZ)*sin(AngleX)*sin(AngleY) - cos(AngleX)*sin(AngleZ)) + Point.Z *(cos(AngleX)*cos(AngleZ)*sin(AngleY) + sin(AngleX)*sin(AngleZ)),
-		Point.X*cos(AngleY)*sin(AngleZ) + Point.Z*(-cos(AngleZ)*sin(AngleX) + cos(AngleX)*sin(AngleY)*sin(AngleZ)) + Point.Y*(cos(AngleX)*cos(AngleZ) + sin(AngleX)*sin(AngleY)*sin(AngleZ)),
-		Point.Z*cos(AngleX)*cos(AngleY) + Point.Y*cos(AngleY)*sin(AngleX) + Point.X*sin(AngleY));
+	return FVector(180 * (Point.X*cos(AngleY)*cos(AngleZ) + Point.Y*(cos(AngleZ)*sin(AngleX)*sin(AngleY) - cos(AngleX)*sin(AngleZ)) + Point.Z *(cos(AngleX)*cos(AngleZ)*sin(AngleY) + sin(AngleX)*sin(AngleZ))) / PI,
+		180 * (Point.X*cos(AngleY)*sin(AngleZ) + Point.Z*(-cos(AngleZ)*sin(AngleX) + cos(AngleX)*sin(AngleY)*sin(AngleZ)) + Point.Y*(cos(AngleX)*cos(AngleZ) + sin(AngleX)*sin(AngleY)*sin(AngleZ))) / PI,
+		180 * (Point.Z*cos(AngleX)*cos(AngleY) + Point.Y*cos(AngleY)*sin(AngleX) + Point.X*sin(AngleY)) / PI);
 
 }
 
 FVector AProceduralCubeActor::UnRotateOnXYZ(FVector Point, float AngleX, float AngleY, float AngleZ)
 {
-	return FVector(Point.X*cos(AngleY)*cos(AngleZ) - Point.Z*sin(AngleY) + Point.Y *cos(AngleY)*sin(AngleZ),
-		Point.Z*cos(AngleY)*sin(AngleX) + Point.X*(cos(AngleZ)*sin(AngleX)*sin(AngleY) - cos(AngleX)*sin(AngleZ)) + Point.Y*(cos(AngleX)*cos(AngleZ) + sin(AngleX)*sin(AngleY)*sin(AngleZ)),
-		Point.Z*cos(AngleX)*cos(AngleY) + Point.X*(cos(AngleX)*cos(AngleZ)*sin(AngleY) + sin(AngleX)*sin(AngleZ)) + Point.Y*(-cos(AngleZ)*sin(AngleX) + cos(AngleX)*sin(AngleY)*sin(AngleZ)));
+	return FVector( 180 * (Point.X*cos(AngleY)*cos(AngleZ) - Point.Z*sin(AngleY) + Point.Y *cos(AngleY)*sin(AngleZ))/PI,
+		180 * (Point.Z*cos(AngleY)*sin(AngleX) + Point.X*(cos(AngleZ)*sin(AngleX)*sin(AngleY) - cos(AngleX)*sin(AngleZ)) + Point.Y*(cos(AngleX)*cos(AngleZ) + sin(AngleX)*sin(AngleY)*sin(AngleZ)))/PI,
+		180 * (Point.Z*cos(AngleX)*cos(AngleY) + Point.X*(cos(AngleX)*cos(AngleZ)*sin(AngleY) + sin(AngleX)*sin(AngleZ)) + Point.Y*(-cos(AngleZ)*sin(AngleX) + cos(AngleX)*sin(AngleY)*sin(AngleZ))) / PI);
 }
 
 // ------------------------------------------------ End - ROTATION FUNCTIONS ------------------------------------------------ \\
--- Source/ProceduralMesh/ProceduralCubeActor.h
+++ Source/ProceduralMesh/ProceduralCubeActor.h
@@ -16,60 +16,109 @@ class PROCEDURALMESH_API AProceduralCubeActor : public AActor
 	GENERATED_UCLASS_BODY()
 
 public:
-	// Allow viewing/changing the Material ot the procedural Mesh in editor (if placed in a level at construction)
+
+	// --------------------------------------- BEGIN PROPERTIES --------------------------------------- \\
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	TSubobjectPtr<UProceduralMeshComponent> mesh;
 
-	void GenerateCube(const float& InSize, TArray<FProceduralMeshTriangle>& OutTriangles);
-
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p0;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p1;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p2;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p3;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p4;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p5;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p6;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FVector p7;
 
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v0;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v1;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v2;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v3;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v4;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v5;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v6;
+
 	UPROPERTY(BlueprintReadWrite, Category = Materials)
 	FProceduralMeshVertex v7;
 
+	UPROPERTY(BlueprintReadWrite, Category = Materials)
+	TArray<AProceduralCubeActor*> ExtrudedCubes;
+
+	// --------------------------------------- BEGIN GENERATION FUNCTIONS --------------------------------------- \\
+
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	void GenerateCubePs(const float& InSize);
+	void GenerateCube(FVector StarterP0Location, float XSize, float YSize, float ZSize);
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	void GenerateCubePs(FVector P0Coords, float XSize, float YSize, float ZSize);
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
 	void GenerateCubeVs();
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	void SetCubeVColors(FColor VertexColor);
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
 	void GenerateCubeFace(FProceduralMeshVertex GivenV0, FProceduralMeshVertex GivenV1, FProceduralMeshVertex GivenV2, FProceduralMeshVertex GivenV3, FProceduralMeshTriangle& t1, FProceduralMeshTriangle& t2);
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	void ExtrusionFromGivenFaceVertexes(AProceduralCubeActor* NewCube, TArray<FProceduralMeshVertex> FaceVertexes);
+
+	// --------------------------------------- BEGIN VERTEX FUNCTIONS --------------------------------------- \\
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
 	FProceduralMeshVertex MoveVertexP0(FVector Direction, FProceduralMeshVertex Vertex, FRotator CubeRotation);
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	FVector FindAndMoveVertex(FVector MovementDirection, FProceduralMeshVertex VertexToMove, TArray<FProceduralMeshTriangle>& CurrentTriangles);
+	
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	void UpdateCubeVertexLocation(FProceduralMeshVertex VertexToUpdate);
+
+	// --------------------------------------- BEGIN FACES and ARROWS FUNCTIONS --------------------------------------- \\
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
 	TArray<FProceduralMeshVertex> MoveFace(float MovementSign, TArray<FProceduralMeshVertex> VertexesArray);
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	FVector CalculateArrowLocationFromFaceVertex(TArray<FProceduralMeshVertex> FaceVertexes);
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	TArray<FProceduralMeshVertex> FindFaceVertexesFromArrowLocation(FVector ArrowLocation);
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	FRotator GetOrtogonalFaceDirectionFromFaceVertex(FVector GivenLocation, TArray<FProceduralMeshVertex> VertexesArray);
+
+	// --------------------------------------- AUX FUNCTIONS --------------------------------------- \\
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
 	FVector ObtainMovementDirection(FVector Direction, FRotator CubeRotation, FVector VPosition);
@@ -78,12 +127,18 @@ public:
 	FVector UnitVector(FVector GivenVector);
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	FVector FindAndMoveVertex(FVector MovementDirection, FProceduralMeshVertex VertexToMove, TArray<FProceduralMeshTriangle>& CurrentTriangles);
+	float EuclideanDistance(FVector P, FVector Q);
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	void UpdateCubeVertexLocation(FProceduralMeshVertex VertexToUpdate);
+	float VectorModule(FVector GivenVector);
 
-	// - BEGIN - Rotations
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	FVector CrossProd(FVector U, FVector V);
+
+	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
+	bool VectorContainsElement(TArray<int32> GivenVector, int32 GivenElement);
+
+	// --------------------------------------- ROTATION FUNCTIONS --------------------------------------- \\
 
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
 	FVector RotateOnX(FVector Point, float Angle);
@@ -109,23 +164,4 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
 	FVector UnRotateOnXYZ(FVector Point, float AngleX, float AngleY, float AngleZ);
 
-	// - END - Rotations
-
-	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	FVector CalculateArrowLocationFromFaceVertex(TArray<FProceduralMeshVertex> FaceVertexes);
-
-	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	float EuclideanDistance(FVector P, FVector Q);
-
-	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	TArray<FProceduralMeshVertex> FindFaceVertexesFromArrowLocation(FVector ArrowLocation);
-
-	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	FRotator GetOrtogonalFaceDirectionFromFaceVertex(FVector GivenLocation, TArray<FProceduralMeshVertex> VertexesArray);
-
-	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	float VectorModule(FVector GivenVector);
-
-	UFUNCTION(BlueprintCallable, Category = "Components|ProceduralMesh")
-	FVector CrossProd(FVector U, FVector V);
 };
